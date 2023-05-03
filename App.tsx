a4661116329792d5796f5bfd14ebba7a2fa6779e
Alex HM <alex13pks@gmail.com>
2023-05-03 01:34:12 -0600

    Add theme background

--- App.tsx
+++ App.tsx
@@ -1,39 +1,51 @@
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import Home from './pages/Home'
-import CharactersPage from './pages/CharactersPage'
-import { Header } from './components'
-import EpisodeDetails from './pages/EpisodeDetails'
+import 'react-native-gesture-handler';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './pages/Home';
+import CharactersPage from './pages/CharactersPage';
+import { Header } from './components';
+import EpisodeDetails from './pages/EpisodeDetails';
 
-const Drawer = createDrawerNavigator()
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
+  },
+  dark: false,
+};
 
-export default function App (): React.ReactElement {
+const Drawer = createDrawerNavigator();
+
+export default function App(): React.ReactElement {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" screenOptions={{
-        header: ({ navigation }) => {
-          return <Header navigation={navigation} />
-        },
-        drawerPosition: 'right',
-        drawerActiveBackgroundColor: 'transparent',
-        drawerStyle: {
-          marginTop: 100,
-          backgroundColor: '#f5f5f5',
-          width: '100%'
-        },
-        drawerLabelStyle: {
-          fontSize: 30,
-          textAlign: 'center',
-          marginRight: -32
-        },
-        overlayColor: 'transparent',
-        drawerType: 'front'
-      }}>
+    <NavigationContainer theme={MyTheme}>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          header: ({ navigation }) => {
+            return <Header navigation={navigation} />;
+          },
+          drawerPosition: 'right',
+          drawerActiveBackgroundColor: 'transparent',
+          drawerStyle: {
+            marginTop: 100,
+            backgroundColor: '#fff',
+            width: '100%',
+          },
+          drawerLabelStyle: {
+            fontSize: 30,
+            textAlign: 'center',
+            marginRight: -32,
+          },
+          overlayColor: 'transparent',
+          drawerType: 'front',
+        }}
+      >
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Characters" component={CharactersPage} />
-        <Drawer.Screen name="EpisodeDetails" component={EpisodeDetails}/>
+        <Drawer.Screen name="EpisodeDetails" component={EpisodeDetails} />
       </Drawer.Navigator>
     </NavigationContainer>
-  )
+  );
 }
